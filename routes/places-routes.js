2e330fdd5d61534fb69402f508de4d0864d46353
Jimmy Svensson <jimmy888swe@gmail.com>
2021-03-03 23:29:16 +0100

    fix: func and dummy data

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,4 +2,30 @@ const express = require('express');
 
 const router = express.Router();
 
+const DUMMY_DATA = [
+  {
+    id: '1',
+    title: 'My house',
+    description: 'were I live',
+    location: {
+      lat:57.6978115,
+      lgn:12.2831768
+    },
+    address: 'Legendvägen 36',
+    creator: 'u1'
+  }
+];
+
+router.get('/:id', (req, res, next) => {
+
+  const placeId = req.params.id;
+  console.log(placeId);
+  const place = DUMMY_DATA.find(p => {
+    return p.id === placeId;
+  });
+  console.log(place);
+  console.log('hello from GET in places');
+  res.json({place});
+});
+
 module.exports = router;
