81779bff00bb0efbcd7ab0648617725fb5eabd73
Jimmy Svensson <jimmy888swe@gmail.com>
2021-03-04 23:19:43 +0100

    fest: default error middelware

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ const app = express();
 app.use('/api/places', placesRoute);
 app.use('/api/users', usersRoute);
 
+app.use((error, req ,res ,next) => {
+  if(res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({message:error.message || 'An unkown error occerrd!'})
+});
+
 app.listen(5000);
