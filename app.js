874e129bed6696d62135749f419e9f8ca5ef9875
Jimmy Svensson <jimmy888swe@gmail.com>
2021-03-07 16:38:45 +0100

    feat: error on wrong path

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const HttpError = require('./models/http.error');
+
 const placesRoute = require('./routes/places-routes');
 const usersRoute = require('./routes/users-routes');
 
@@ -9,6 +11,11 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoute);
 app.use('/api/users', usersRoute);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404);
+    throw error;
+});
 
 app.use((error, req ,res ,next) => {
   if(res.headerSent) {
